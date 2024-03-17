c05d3a859e8c084913193260298b1f0edb724651
james <nyamuj59@gmail.com>
2024-03-17 11:27:26 +0300

    completed the services section

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,11 +74,19 @@ const modalViews = document.querySelectorAll('.services__modal'),
   modalCloses = document.querySelectorAll('.services__modal-close')
 
 let modal = function (modalClick) {
-  modalViews[modalClick].classList.add('active__modal')
+  modalViews[modalClick].classList.add('active-modal')
 }
 
 modalBtns.forEach((modalBtn, i) => {
   modalBtn.addEventListener('click', () => {
     modal(i)
+  }) 
+})
+
+modalCloses.forEach((modalClose)=>{
+  modalClose.addEventListener('click', ()=>{
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal")
+    })
   })
 })
