98810f89a65764b46edf3b534a6ef2e7c702ef1a
james <nyamuj59@gmail.com>
2024-03-18 18:17:05 +0300

    completed the main and footer of the site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,92 +1,126 @@
 /*=============== MENU SHOW Y HIDDEN ============*/
-const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close")
 
 /*=============== SHOW MENU =====================*/
 /* validate if constant exists */
 if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu')
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu")
   })
 }
 
 /*=============== HIDE MENU =====================*/
 /* validate if constant exists */
 if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu')
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu")
   })
 }
 
 /*============ REOMOVE MENU ON LINK CLICK ============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link")
 
 function linkAction() {
-  const navMenu = document.getElementById('nav-menu')
+  const navMenu = document.getElementById("nav-menu")
   //we remove the nav menu whenever we click on a link
-  navMenu.classList.remove('show-menu')
+  navMenu.classList.remove("show-menu")
 }
-navLink.forEach((n) => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction))
 
 /*============ SKILLS TOGGLING ======================*/
-const skillsContent = document.getElementsByClassName('skills__content'),
-  skillsHeader = document.querySelectorAll('.skills__header')
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header")
 
 function toggleSkills() {
   let itemCalss = this.parentNode.className
 
   for (i = 0; i < skillsContent.length; i++) {
-    skillsContent[i].className = 'skills__content skills__close'
+    skillsContent[i].className = "skills__content skills__close"
   }
-  if (itemCalss === 'skills__content skills__close') {
-    this.parentNode.className = 'skills__content skills__open'
+  if (itemCalss === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open"
   }
 }
 
 skillsHeader.forEach((el) => {
-  el.addEventListener('click', toggleSkills)
+  el.addEventListener("click", toggleSkills)
 })
 
 /*========================= QUALIFICATIONS TAB ======================*/
-const tabs = document.querySelectorAll('[data-target]'),
-  tabsContent = document.querySelectorAll('[data-content]')
+const tabs = document.querySelectorAll("[data-target]"),
+  tabsContent = document.querySelectorAll("[data-content]")
 
 tabs.forEach((tab) => {
-  tab.addEventListener('click', () => {
+  tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.target)
 
     tabsContent.forEach((tabContent) => {
-      tabContent.classList.remove('qualification__active')
+      tabContent.classList.remove("qualification__active")
     })
-    target.classList.add('qualification__active')
+    target.classList.add("qualification__active")
 
     tabs.forEach((tab) => {
-      tab.classList.remove('qualification__active')
+      tab.classList.remove("qualification__active")
     })
-    tab.classList.add('qualification__active')
+    tab.classList.add("qualification__active")
   })
 })
 
 /*================================ SERVICES MODAL ========================*/
-const modalViews = document.querySelectorAll('.services__modal'),
-  modalBtns = document.querySelectorAll('.services__button'),
-  modalCloses = document.querySelectorAll('.services__modal-close')
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close")
 
 let modal = function (modalClick) {
-  modalViews[modalClick].classList.add('active-modal')
+  modalViews[modalClick].classList.add("active-modal")
 }
 
 modalBtns.forEach((modalBtn, i) => {
-  modalBtn.addEventListener('click', () => {
+  modalBtn.addEventListener("click", () => {
     modal(i)
-  }) 
+  })
 })
 
-modalCloses.forEach((modalClose)=>{
-  modalClose.addEventListener('click', ()=>{
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
     modalViews.forEach((modalView) => {
       modalView.classList.remove("active-modal")
     })
   })
+})
+
+/*====================== PORTFOLIO SWIPER =======================*/
+
+let swiperPortfolio = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+})
+
+/*======================== PORTFOLIO ===========================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
 })
